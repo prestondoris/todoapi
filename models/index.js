4ebaf37cf620eb41ac803395ad2995c751954fb0
Preston Doris <prestondoris@gmail.com>
2020-03-26 12:00:00 -0700

    Changed DB from local DB to MongoDB Atlas

--- models/index.js
+++ models/index.js
@@ -1,7 +1,17 @@
-var mongoose = require('mongoose');
-mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+const mongoose = require('mongoose');
+const connectionStr = 'mongodb+srv://prestondoris:SB1w9OQnLoqIPLOf@tododb-1w5d5.mongodb.net/TodoDB?retryWrites=true&w=majority'
 
-mongoose.Promise = Promise;
+const start = Date.now();
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 3000,
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+}
+mongoose.connect(connectionStr, opts)
+  .then( () => console.log('Connected to MongoDB') )
+  .catch( error => console.log(`Caught "${error}" after ${Date.now() - start}`))
 
 module.exports.Todo = require("./todo");
